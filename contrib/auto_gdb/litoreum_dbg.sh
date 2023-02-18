3dfa442686844cbf0921e1b46430891c808ce23f
Altcoin-Master <121792914+Altcoin-Master@users.noreply.github.com>
2023-02-18 15:21:11 +0800

    v1.2.0.0

--- contrib/auto_gdb/litoreum_dbg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.litoreumcore/litoreumd.pid file instead
-litoreum_pid=$(<~/.litoreumcore/testnet3/litoreumd.pid)
-sudo gdb -batch -ex "source debug.gdb" litoreumd ${litoreum_pid}
